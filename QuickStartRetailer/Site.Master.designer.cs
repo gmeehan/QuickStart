99eae12674cb9033362c38734ec512f2c812ec5c
gmeehan <graham_meehan@hotmail.com>
2012-10-08 16:49:51 -0700

    Another Test

--- QuickStartRetailer/Site.Master.designer.cs
+++ QuickStartRetailer/Site.Master.designer.cs
@@ -22,6 +22,24 @@ namespace QuickStartRetailer {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder HeadContent;
         
         /// <summary>
+        /// TextBox1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        
+        /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+        
+        /// <summary>
         /// NavigationMenu control.
         /// </summary>
         /// <remarks>
