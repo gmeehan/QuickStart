c55b59cce6d2c182d068f32d74173eae6a1e6367
Marlon <m_mattice@fanshaweonline.ca>
2012-10-09 11:04:45 -0400

    Database Creation Program

---
+++ Documents/FANSHAWE/Y3S2/Managing Software Projects/CreateDB/CreateDB/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace CreateDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreateDatabase = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCreateDatabase
+            // 
+            this.btnCreateDatabase.Location = new System.Drawing.Point(142, 168);
+            this.btnCreateDatabase.Name = "btnCreateDatabase";
+            this.btnCreateDatabase.Size = new System.Drawing.Size(126, 23);
+            this.btnCreateDatabase.TabIndex = 0;
+            this.btnCreateDatabase.Text = "Create Database";
+            this.btnCreateDatabase.UseVisualStyleBackColor = true;
+            this.btnCreateDatabase.Click += new System.EventHandler(this.btnCreateDatabase_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(373, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Please Enter The Location of Where You Would Like Your Database Created";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CreateDB.Properties.Resources.QuickStart_Logo_Transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(107, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(203, 82);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 262);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCreateDatabase);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreateDatabase;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Documents/FANSHAWE/Y3S2/Managing Software Projects/CreateDB/CreateDB/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+
+namespace CreateDB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class MyDVDs : DataContext
+        {
+            public Table<DVD> DVDs;
+            public MyDVDs(string connection) : base(connection) { }
+        }
+
+        [Table(Name = "DVDTable")]
+        public class DVD
+        {
+            [Column(IsPrimaryKey = true)]
+            public string Title;
+            [Column]
+            public string Rating;
+        }
+
+        public void CreateDatabase(string location)
+        {
+            MyDVDs db = new MyDVDs(location);
+            if (db.DatabaseExists())
+            {
+                if (MessageBox.Show("Database already exists. Recreate?", "Recreate Database?", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    db.DeleteDatabase();
+                    db.CreateDatabase();
+                    MessageBox.Show("Database Created!");
+                }
+                else
+                {
+                    MessageBox.Show("Database Not Created");
+                }
+            }
+            else
+            {
+                db.CreateDatabase();
+                MessageBox.Show("Database Created!");
+            }
+        }
+
+        public string ChooseFolder()
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                return folderBrowserDialog1.SelectedPath;
+            }
+            return "";
+        }
+
+        private void btnCreateDatabase_Click(object sender, EventArgs e)
+        {
+            string location = ChooseFolder().Trim();
+            if (location != "")
+            {
+                if (location[location.Length - 1] == '\\')
+                {
+                    location += "test2.mdf";
+                }
+                else
+                {
+                    location += "\\test2.mdf";
+                }
+                try
+                {
+                    CreateDatabase(location);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter the location you would like your database created");
+            }
+        }
+    }
+}
