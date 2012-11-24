34775c8ccdfc29864743e1dc87f90971d54ee9b7
David Leonard <d_leonard2@fanshaweonline.ca>
2012-11-24 15:09:39 -0500

    DLeonard -- added missing files

---
+++ QSR Web Application/QSRDataObjects/InvoiceItemData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QSRDataObjects
+{
+    public class InvoiceItemData : ErrorLoggerData
+    {
+        /// <summary>
+        /// Purpose: Grabs all InvoiceItems via stored procedure 
+        /// Accepts: Nothing
+        /// Returns: List<InvoiceItem>
+        /// </summary>
+        public List<InvoiceItem> GetAllInvoiceItemsByOrderID(int orderid)
+        {
+            QuickStart_DBEntities dbContext;
+            List<InvoiceItem> retList = null;
+            try
+            {
+                dbContext = new QuickStart_DBEntities();
+
+                //all Invoice Items are returned for this order id
+                retList = dbContext.spGetInvoiceItemsByOrderID(orderid).ToList();
+            }
+            catch (Exception ex)
+            {
+                ErrorRoutine(ex, "InvoiceItemData", "GetAllInvoiceItemsByOrderID");
+            }
+
+            return retList;
+        }
+    }
+}
---
+++ QSR Web Application/QSRWebObjects/InvoiceItem.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using QSRDataObjects;
+
+namespace QSRWebObjects
+{
+    //Invoice item is used to display full details of an order item, including its product info and tax info
+    public class InvoiceItem : ErrorLogger
+    {
+        private string _prodCode;
+        private string _prodName;
+        private string _prodDesc;
+        private double _msrp;
+        private int _quantity;
+        private double _tax;
+        private double _total;
+
+        public string ProdCode
+        {
+            get { return _prodCode; }
+            set { _prodCode = value; }
+        }
+
+        public string ProdName
+        {
+            get { return _prodName; }
+            set { _prodName = value; }
+        }
+
+        public string ProdDesc
+        {
+            get { return _prodDesc; }
+            set { _prodDesc = value; }
+        }
+
+        public double Msrp
+        {
+            get { return _msrp; }
+            set { _msrp = value; }
+        }
+
+        public int Quantity
+        {
+            get { return _quantity; }
+            set { _quantity = value; }
+        }
+
+        public double Tax
+        {
+            get { return _tax; }
+            set { _tax = value; }
+        }
+
+        public double Total
+        {
+            get { return _total; }
+            set { _total = value; }
+        }
+
+        public List<InvoiceItem> GetAllInvoiceItemsByOrderID(int orderid)
+        {
+            List<InvoiceItem> invoiceItems = new List<InvoiceItem>();
+            try
+            {
+                InvoiceItemData data = new InvoiceItemData();
+                List<QSRDataObjects.InvoiceItem> dataInvoiceItems = data.GetAllInvoiceItemsByOrderID(orderid);
+
+                foreach (QSRDataObjects.InvoiceItem dataItem in dataInvoiceItems)
+                {
+                    InvoiceItem item = new InvoiceItem();
+                    item.ProdCode = dataItem.ProductCode;
+                    item.ProdName = dataItem.ProductName;
+                    item.ProdDesc = dataItem.ProductDescription;
+                    item.Msrp = dataItem.MSRP;
+                    item.Quantity = dataItem.Quantity.Value;
+                    item.Tax = dataItem.TaxAmount.Value;
+                    invoiceItems.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorRoutine(ex, "InvoiceItem", "GetAllInvoiceItemsByOrderID");
+            }
+            return invoiceItems;
+        }
+    }
+}
